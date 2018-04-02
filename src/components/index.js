e83ac85e42db2cf8f4890134c622c6aedf08b64c
scotthollandjr <scott.holland@eyecuelab.com>
2018-04-01 23:03:26 -0700

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './container';
-export * from './gameForm';
-export * from './gameList';
-export * from './playerList';
-export * from './teamList';
--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const apiKey = "AIzaSyCzUDpQ1RnSYH3kdmvRRviNQEzyCUv6FQo";
-export const authDomain = "rocketplanet-d5934.firebaseapp.com";
-export const databaseURL = "https://rocketplanet-d5934.firebaseio.com";
-export const projectId = "rocketplanet-d5934";
-export const storageBucket = "rocketplanet-d5934.appspot.com";
-export const messagingSenderId = "543879339561";
