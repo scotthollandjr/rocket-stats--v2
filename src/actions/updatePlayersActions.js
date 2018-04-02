9ebd198943ffc8d9ca265ac84d6088e9a8d8f189
scotthollandjr <scott.holland@eyecuelab.com>
2018-04-01 22:25:58 -0700

    Update player stats on submitted game

--- src/actions/updatePlayersActions.js
+++ src/actions/updatePlayersActions.js
@@ -4,17 +4,49 @@ import database from '../database';
 export function updatePlayers(players) {
   return dispatch => {
     dispatch(updatePlayersRequestedAction());
-    console.log("players", players)
-    const savesRef = database.ref('/players/2/saves');
-    savesRef.transaction(function(currentSaves) {
-      return currentSaves + 1;
-    })
-    .then(() => {
-      dispatch(updatePlayersRequestedAction({players}));
-    })
-    .catch((error) => {
-      dispatch(updatePlayersRequestedAction());
-    })
+
+    // LOOP THROUGH PLAYERS
+    for (let i in players) {
+      const playerUpdate = players[i];
+      const playerRef = database.ref('/players/' + playerUpdate.id);
+
+      // SET BLANK STAT PLACEHOLDERS
+      let assists = 0;
+      let goals = 0;
+      let mvps = 0;
+      let saves = 0;
+      let score = 0;
+      let shots = 0;
+      let wins = 0;
+
+      // GET CURRENT STATS FROM DB
+      playerRef.once("value").then(function(snapshot) {
+        assists = snapshot.child("assists").val();
+        goals = snapshot.child("goals").val();
+        mvps = snapshot.child("mvps").val();
+        saves = snapshot.child("saves").val();
+        score = snapshot.child("score").val();
+        shots = snapshot.child("shots").val();
+        wins = snapshot.child("wins").val();
+      }).then(function() {
+        // ADD CURRENT STATS TO NEW STATS
+        playerRef.update({
+          assists: assists + playerUpdate.assists,
+          goals: goals + playerUpdate.goals,
+          mvps: mvps + (playerUpdate.mvp ? 1 : 0),
+          saves: saves + playerUpdate.saves,
+          score: score + playerUpdate.score,
+          shots: shots + playerUpdate.shots,
+          wins: wins + (playerUpdate.win ? 1 : 0)
+        })
+      })
+    }
+    // .then(() => {
+    //   dispatch(updatePlayersRequestedAction({players}));
+    // }) 
+    // .catch((error) => {
+    //   dispatch(updatePlayersRequestedAction());
+    // })
   }
 }
 
