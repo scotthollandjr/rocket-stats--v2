5b35d57e8c84875f3b4fe029052f0cdc89d31d16
scotthollandjr <scott.holland@eyecuelab.com>
2018-04-12 18:23:54 -0700

    Style player list

--- src/components/playerList/index.js
+++ src/components/playerList/index.js
@@ -11,7 +11,9 @@ class PlayerList extends React.Component {
   }
 
   componentWillMount() {
-    this.setState({ players: this.props.players });
+    this.setState({ players: this.props.players }, function() {
+      this.playerSort("score");
+    });
   }
 
   playerSort(key) {
@@ -30,34 +32,79 @@ class PlayerList extends React.Component {
     return Math.round((player[stat] / player.games) * 100) / 100;
   }
 
+  calcOverall(player) {
+    let overall = 0;
+    overall = 
+      (player.goals * 1) +
+      (player.assists * .75) +
+      (player.saves * .6) +
+      (player.shots * .4) +
+      ((player.goals/player.shots) * .5);
+    overall = overall * (1/player.games)
+    return Math.round(overall * 100) / 100
+  }
+
   renderPlayers(item, i) {
     return (
-      <div key={i} className="player-card">
-        <div className="inner-card">
+      <div key={i} className="player-row">
+        <div className="avatar-container">
+          {/*<div>{item.name}</div>*/}
+          <img src={item.avatar} className="avatar" />
+        </div>
+        <div className="stat-rows">
           <div className="stat-row">
-            <div className="stat-col">
-              <div className="avatar-container">
-                <img src={item.avatar} className="avatar" />
-              </div>
-              <div>{item.name}</div>
+            <div className="stat-cell">
+              <span>{item.goals}</span>
             </div>
-            <div className="stat-col">
-              <div>goals: {item.goals} ({this.perGame(item, 'goals')})</div>
-              <div>shots: {item.shots} ({this.perGame(item, 'shots')})</div>
-              <div>accuracy: {Math.round((item.goals / item.shots) * 100) / 100}</div>
+            <div className="stat-cell">
+              <span>{item.shots}</span>
             </div>
-            <div className="stat-col">
-              <div>saves: {item.saves} ({this.perGame(item, 'saves')})</div>
-              <div>assists: {item.assists} ({this.perGame(item, 'assists')})</div>
+            <div className="stat-cell">
+              <span>{Math.round((item.goals / item.shots) * 100) / 100}</span>
             </div>
-            <div className="stat-col">
-              <div>wins: {item.wins} ({this.perGame(item, 'wins')})</div>
-              <div>mvps: {item.mvps} ({this.perGame(item, 'mvps')})</div>
+            <div className="stat-cell">
+              <span>{item.saves}</span>
             </div>
-            <div className="stat-col">
-              <div>score: {item.score} ({this.perGame(item, 'score')})</div>
-              <div>games: {item.games}</div>
+            <div className="stat-cell">
+              <span>{item.assists}</span>
             </div>
+            <div className="stat-cell">
+              <span>{item.wins}</span>
+            </div>
+            <div className="stat-cell">
+              <span>{item.mvps}</span>
+            </div>
+            <div className="stat-cell">
+              <span>{item.score}</span>
+            </div>
+            {/*<div>{item.games}</div>*/}
+          </div>
+          <div className="stat-row">
+            <div className="stat-cell fade">
+              <span>{this.perGame(item, 'goals')}</span>
+            </div>
+            <div className="stat-cell fade">
+              <span>{this.perGame(item, 'shots')}</span>
+            </div>
+            <div className="stat-cell fade">
+              <span>{Math.round((item.goals / item.shots) * 100) / 100}</span>
+            </div>
+            <div className="stat-cell fade">
+              <span>{this.perGame(item, 'saves')}</span>
+            </div>
+            <div className="stat-cell fade">
+              <span>{this.perGame(item, 'assists')}</span>
+            </div>
+            <div className="stat-cell fade">
+              <span>{this.perGame(item, 'wins')}</span>
+            </div>
+            <div className="stat-cell fade">
+              <span>{this.perGame(item, 'mvps')}</span>
+            </div>
+            <div className="stat-cell fade">
+              <span>{this.perGame(item, 'score')}</span>
+            </div>
+            {/*<div>{item.games}</div>*/}
           </div>
         </div>
       </div>
@@ -74,17 +121,17 @@ class PlayerList extends React.Component {
     } else {
       return (
         <div>
+          <div className="sort-buttons">
+            <a href="#" className="sort-button" onClick={() => this.playerSort("goals")}>goals</a>
+            <a href="#" className="sort-button" onClick={() => this.playerSort("shots")}>shots</a>
+            <a href="#" className="sort-button" onClick={() => this.playerSort("accuracy")}>accuracy</a>
+            <a href="#" className="sort-button" onClick={() => this.playerSort("saves")}>saves</a>
+            <a href="#" className="sort-button" onClick={() => this.playerSort("assists")}>assists</a>
+            <a href="#" className="sort-button" onClick={() => this.playerSort("wins")}>wins</a>
+            <a href="#" className="sort-button" onClick={() => this.playerSort("mvps")}>mvps</a>
+            <a href="#" className="sort-button" onClick={() => this.playerSort("score")}>score</a>
+          </div>
           <div className="card-container">
-            <div>
-              <button onClick={() => this.playerSort("games")}>games</button>
-              <button onClick={() => this.playerSort("score")}>score</button>
-              <button onClick={() => this.playerSort("assists")}>assists</button>
-              <button onClick={() => this.playerSort("saves")}>saves</button>
-              <button onClick={() => this.playerSort("shots")}>shots</button>
-              <button onClick={() => this.playerSort("wins")}>wins</button>
-              <button onClick={() => this.playerSort("mvps")}>mvps</button>
-              <button onClick={() => this.playerSort("accuracy")}>accuracy</button>
-            </div>
             {
               Object.keys(this.state.players).map((player, i) => {
                 return (
